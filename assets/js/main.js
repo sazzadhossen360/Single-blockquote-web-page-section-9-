7fa6ba7c28c84b6d46453adbaf06f8617fbac81a
sazzadhossen360 <63567193+sazzadhossen360@users.noreply.github.com>
2020-04-26 12:00:51 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items:3,
+        margin:30,
+        autoplay:true,
+        loop:true
+
+    });
+  });
+
+    // (document).ready(function($){
+    //     $('.owl-carousel').owlCarouse({
+    //         items:3,
+    //         margin:30,
+    //         nav:false,
+    //         dots:true,
+    //         loop:true
+
+
+    //     });
+    // });
+    
